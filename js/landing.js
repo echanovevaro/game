93576da65c4cdebd796ea61aeab9a93edfcf5c7b
Alvaro <echanovevaro@gmail.com>
2018-01-25 20:22:11 +0100

    cambio de estilos

--- js/landing.js
+++ js/landing.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
-    var gameJson = localStorage["game"];
+	var gameJson = localStorage["game"];
+	$("#fwidgets h2").empty();
     if(gameJson){
         var game =JSON.parse(gameJson);
-        drawGameOver(game);
+		drawGameOver(game);
+		$("#fwidgets h2").text("Juego terminado");
         $("#game-over").show();
         $("#start").hide();
-    } else {
+	} else {
+		$("#fwidgets h2").text("Empezar");
         $("#start").show();
         $("#game-over").hide();
     }
@@ -29,7 +32,7 @@ function drawGameOver(game) {
 			return b.game.score - a.game.score;
 		});
 		shortedPlayers.forEach(function (player, i) {
-			$("#game-over .modal-body").append("<h3>" + parseInt(i + 1) + ".  " + player.name + " con " + player.game.score + " puntos</h3>");
+			$("#game-over .player-list").append("<h3>" + parseInt(i + 1) + ".  " + player.name + " con " + player.game.score + " puntos</h3>");
 		});
 		if (shortedPlayers[0].game.score === shortedPlayers[1].game.score) {
 			$("#game-over h1").text("Ha habido un empate ");
@@ -38,6 +41,6 @@ function drawGameOver(game) {
 		}
 	} else {
 		$("#game-over h1").text("Has ganado!");
-		$("#game-over .modal-body").append("<h3>Puntos acumulados " + game.actualPlayer.game.score + "</h3>");
+		$("#game-over .player-list").append("<h3>Puntos acumulados " + game.actualPlayer.game.score + "</h3>");
 	}
 }
